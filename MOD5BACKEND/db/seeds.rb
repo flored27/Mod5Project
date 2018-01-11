9280305fa9b662b562d466b13c6890b4ffdae44b
flored27 <dflores@fandm.edu>
2018-01-11 09:10:21 -0500

    added faker seed data for tenant, prop, apart

--- MOD5BACKEND/db/seeds.rb
+++ MOD5BACKEND/db/seeds.rb
@@ -6,6 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 20.times do
-Property.create(name: Faker::Name.name,
-            email: Faker::Internet.free_email)
+  Property.create(city: Faker::Address.city,
+                  street_address: Faker::Address.street_address,
+                  zip: Faker::Address.zip,
+                  state: Faker::Address.state,
+                  taxes: Faker::Number.number(5),
+                  mortgage: Faker::Number.number(5)
+                  )
+end
+
+20.times do
+  Tenant.create(name: Faker::Name.name,
+                age: Faker::Number.number(2),
+                phone: Faker::Number.number(10),
+                email: Faker::Internet.email
+                )
+end
+
+20.times do
+  Apartment.create(number: Faker::Number.number(2),
+                  tenant_id: Faker::Number.number(1),
+                  property_id: Faker::Number.number(1)
+                  )
 end

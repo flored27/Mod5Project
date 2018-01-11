faf32e77508ad515b42b14a5bc9061192f856cb6
flored27 <dflores@fandm.edu>
2018-01-11 08:39:58 -0500

    took out county and added zip in properties

--- MOD5BACKEND/db/schema.rb
+++ MOD5BACKEND/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111133207) do
+ActiveRecord::Schema.define(version: 20180111133931) do
 
   create_table "apartments", force: :cascade do |t|
     t.integer "number"
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 20180111133207) do
     t.integer "taxes"
     t.integer "mortgage"
     t.string "state"
-    t.string "county"
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "street_address"
+    t.string "zip"
   end
 
   create_table "tenants", force: :cascade do |t|

75af1e8a307e66fcdd55b55727ab532a8a25da3b
flored27 <dflores@fandm.edu>
2018-01-11 10:46:34 -0500

    prop blngs_to LL, LL has_many aprtm thru prop, has_many tnts thru aprtm

--- MOD5BACKEND/app/models/landlord.rb
+++ MOD5BACKEND/app/models/landlord.rb
@@ -1,5 +1,7 @@
 class Landlord < ApplicationRecord
   has_many :properties
+  has_many :apartments, through: :properties
+  has_many :tenants, through: :apartments
   #not sure if it should be has many apartments, and then has many tenants through apartments
-  
+
 end
--- MOD5BACKEND/app/models/property.rb
+++ MOD5BACKEND/app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
   has_many :apartments
   has_many :tenants, through: :apartments
+  belongs_to :landlord
 end

e5240e65bf842e53c3299ff305f2d3100bee3d4c
flored27 <dflores@fandm.edu>
2018-01-11 08:12:33 -0500

    tenant has_one apart/prop, prop has_many apart/tenants

--- MOD5BACKEND/app/models/property.rb
+++ MOD5BACKEND/app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+  has_many :apartments
+  has_many :tenants, through: :apartments
 end
--- MOD5BACKEND/app/models/tenant.rb
+++ MOD5BACKEND/app/models/tenant.rb
@@ -1,2 +1,5 @@
 class Tenant < ApplicationRecord
+  has_one :apartment
+  has_one :property, through: :apartment
+  #not sure if tenant belongs to or has one landlord
 end

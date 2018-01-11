2e31e152791751ad380efb3c38bef673b8d1f55a
flored27 <dflores@fandm.edu>
2018-01-11 08:19:36 -0500

    added index,show,create,update,destroy, and private params to properties

--- MOD5BACKEND/app/controllers/properties_controller.rb
+++ MOD5BACKEND/app/controllers/properties_controller.rb
@@ -1,2 +1,49 @@
 class PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :update, :destroy]
+
+  # GET /properties
+  def index
+    @properties = Property.all
+    render json: @properties
+  end
+
+  # GET properties/1
+  def show
+    render json: @property
+  end
+
+  # POST /property
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      render json: @property,status: :created, property: @property
+    else
+      render json: @property.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /properties/1
+  def update
+    @property = Property.find(params[:id])
+    @property.update(property_params)
+    if @property.save
+      render json: @property
+    else
+      render json: @property.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @property.destroy
+  end
+
+  private
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:taxes, :mortgage, :location, :state, :county, :city)
+  end
+
 end

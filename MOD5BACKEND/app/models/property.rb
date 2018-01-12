61985a1f8b6ea4697a5db8c71cb19acae66d9d53
flored27 <dflores@fandm.edu>
2018-01-12 10:00:23 -0500

    added comments and validates email, presence, uniqueness = true

--- MOD5BACKEND/app/models/property.rb
+++ MOD5BACKEND/app/models/property.rb
@@ -1,5 +1,7 @@
 class Property < ApplicationRecord
   has_many :apartments, dependent: :destroy
+  # apparently so that when you destroy a property, 
+  # you destroy the apartment/tenant info too
   has_many :tenants, through: :apartments, dependent: :destroy
   belongs_to :landlord
 end

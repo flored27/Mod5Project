f16f408b929fc765dd1b779aa251bd3c9ab21935
flored27 <dflores@fandm.edu>
2018-01-12 09:56:01 -0500

    added dependent: :destroy to property & LL models

--- MOD5BACKEND/app/models/landlord.rb
+++ MOD5BACKEND/app/models/landlord.rb
@@ -1,5 +1,5 @@
 class Landlord < ApplicationRecord
-  has_many :properties
+  has_many :properties, dependent: :destroy
   has_many :apartments, through: :properties
   has_many :tenants, through: :apartments
   #not sure if it should be has many apartments, and then has many tenants through apartments
--- MOD5BACKEND/app/models/property.rb
+++ MOD5BACKEND/app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
-  has_many :apartments
-  has_many :tenants, through: :apartments
+  has_many :apartments, dependent: :destroy
+  has_many :tenants, through: :apartments, dependent: :destroy
   belongs_to :landlord
 end

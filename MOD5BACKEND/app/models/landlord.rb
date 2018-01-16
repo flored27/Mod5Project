35887291d0117ee7254a3a34e9053e09166fb6e5
flored27 <dflores@fandm.edu>
2018-01-16 18:14:12 -0500

    added has_attached_file to apartment, landlord, property, and tenant

--- MOD5BACKEND/app/models/landlord.rb
+++ MOD5BACKEND/app/models/landlord.rb
@@ -8,4 +8,6 @@ class Landlord < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   # so that users have to register with a new email
   has_secure_password #to encrypt passwords
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
--- MOD5BACKEND/app/models/tenant.rb
+++ MOD5BACKEND/app/models/tenant.rb
@@ -2,4 +2,6 @@ class Tenant < ApplicationRecord
   has_one :apartment
   has_one :property, through: :apartment
   #not sure if tenant belongs to or has one landlord
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end

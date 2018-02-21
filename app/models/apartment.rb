64642cc153116817747a5a0169ac06c4fe3b1a0d
flored27 <dflores@fandm.edu>
2018-02-20 23:28:13 -0500

    added dependent destroy

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,5 +1,5 @@
 class Apartment < ApplicationRecord
-  has_many :tenants
+  has_many :tenants, dependent: :destroy
   belongs_to :property
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -2,8 +2,8 @@ class Landlord < ApplicationRecord
   has_many :properties, dependent: :destroy
   # supposedly so that if you destroy a LL,
   # you also destroy the properties assigned to it
-  has_many :apartments, through: :properties
-  has_many :tenants, through: :apartments
+  has_many :apartments, through: :properties, dependent: :destroy
+  has_many :tenants, through: :apartments, dependent: :destroy
   #not sure if it should be has many apartments, and then has many tenants through apartments
   validates :email, presence: true, uniqueness: true
   # so that users have to register with a new email

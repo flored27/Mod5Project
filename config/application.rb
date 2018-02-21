aef1e7e54584fec023adf3cd923ff148a894c856
flored27 <dflores@fandm.edu>
2018-02-21 01:20:05 -0500

    just kept actionmaier perform delivieries, deliverymethod, and settings, put password/email directly

--- config/application.rb
+++ config/application.rb
@@ -31,24 +31,16 @@ module MOD5BACKEND
     config.api_only = true
 
     config.action_mailer.perform_deliveries = true
-
-    # Don't care if the mailer can't send.
-    config.action_mailer.raise_delivery_errors = true
-
-    config.action_mailer.perform_caching = false
-
     config.action_mailer.delivery_method = :smtp
-
     config.action_mailer.smtp_settings = {
-
-
+      :user_name => 'app87031585@heroku.com',
+      :password => 'lt9r9scu7520'
       :domain => "heroku.com",
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
       :enable_starttls_auto => true,
-      :user_name => Rails.application.secrets.email_user_name,
-      :password => Rails.application.secrets.email_password
+
 
       # domain: "127.0.0.1",
       # authentication: "login",

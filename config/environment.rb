6c28232f8a540f693f52e5f8d069ac3ca6644ae3
flored27 <dflores@fandm.edu>
2018-02-21 00:59:13 -0500

    pushed actionmailer configure from development and production to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,33 @@
 # Load the Rails application.
 require_relative 'application'
 
+Invoicer::Application.configure do
+config.action_mailer.perform_deliveries = true
+
+# Don't care if the mailer can't send.
+config.action_mailer.raise_delivery_errors = true
+
+config.action_mailer.perform_caching = false
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+
+  :user_name => ENV['app87031585@heroku.com'],
+  :password => ENV['lt9r9scu7520'],
+  :domain => "heroku.com",
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+
+  # domain: "127.0.0.1",
+  # authentication: "login",
+  # user_name: Rails.application.secrets.email_user_name,
+  # password: Rails.application.secrets.email_password
+}
+end
+
+
 # Initialize the Rails application.
 Rails.application.initialize!

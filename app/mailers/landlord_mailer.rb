a242b09bd5695cfbb85637224a6265729f7aaeeb
flored27 <dflores@fandm.edu>
2018-03-06 22:57:32 -0500

    changed welcome message for landlordmailer email

--- app/mailers/landlord_mailer.rb
+++ app/mailers/landlord_mailer.rb
@@ -4,9 +4,9 @@ class LandlordMailer < ApplicationMailer
   def welcome_email(user)
 
     @user = user
-    @specialtext = "HEYEYEY"
+    @specialtext = `Hello #{user}, welcome to Property Assistant!`
     @url  = 'http://gmail.fandm.edu'
-    mail(to: 'diego.flores@fandm.edu', subject: 'Welcome to My Awesome Site')
+    mail(to: 'diego.flores@fandm.edu', subject: 'Welcome to Property Assistant 2018')
   end
 
   def message_tenant(user)

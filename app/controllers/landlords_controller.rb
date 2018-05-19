504ab6893649cdbebf2e3118c734499b982a7337
flored27 <dflores@fandm.edu>
2018-05-18 22:47:16 -0400

    added console.log to test for Landlord.create

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -12,31 +12,29 @@ before_action :set_landlord, only: [:show, :destroy]
     render json: @landlord
   end
 
-  # POST /landlord
   def create
-
     @landlord = Landlord.new(landlord_params)
     if @landlord.save
-
+      console.log("Welcome, success")
       LandlordMailer.welcome_email(@landlord).deliver_now
-
-      render json: {landlord: @landlord, jwt: issue_token(@landlord.id)}, status: :created, landlord: @landlord
-    else
-      render json: @landlord.errors, status: :unprocessable_entity
-    end
+    #   render json: {landlord: @landlord, jwt: issue_token(@landlord.id)}, status: :created, landlord: @landlord
+    # else
+    #   render json: @landlord.errors, status: :unprocessable_entity
+    # end
   end
 
-  # PATCH/PUT /landlords/1
   def update
-      @landlord = Landlord.find(params[:id])
+    @landlord = Landlord.find(params[:id])
     @landlord.update(landlord_params)
     if @landlord.save
       LandlordMailer.message_tenant(@landlord).deliver_now
-      render json: @landlord
-    else
-      render json: @landlord.errors, status: :unprocessable_entity
-    end
+    #   render json: @landlord
+    # else
+    #   render json: @landlord.errors, status: :unprocessable_entity
+    # end
   end
+
+
 
   def destroy
     @landlord.destroy

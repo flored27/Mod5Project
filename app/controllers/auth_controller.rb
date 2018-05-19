4ca2dae68129d5497a15f8d9639f7383a2502323
flored27 <dflores@fandm.edu>
2018-05-18 23:35:48 -0400

    added console.log("Welcome, success" to authcontroller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,6 +2,7 @@ class AuthController < ApplicationController
   def create
     user = Landlord.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      console.log("Welcome, success")
 
       render json: {
         id: user.id,

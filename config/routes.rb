3184987a0b38a99b7df73d056ee1dc160e4a39cb
flored27 <dflores@fandm.edu>
2018-05-20 16:07:11 -0400

    fixed routes syntax error for '/find'

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :apartments
   post '/login', to: 'auth#create'
   get '/current_user', to: 'auth#show'
-  get '/find' to: 'auth#find'
+  get '/find', to: 'auth#find'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
